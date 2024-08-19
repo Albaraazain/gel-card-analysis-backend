4dab5a6d020119cf82b86fe208cf229ee7823b43
Albaraa Taher Zeinoglu <65316209+Albaraazain@users.noreply.github.com>
2024-08-19 15:38:16 +0300

    allow varcel frontend to access

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import base64
 
 app = Flask(__name__)
 # Update CORS configuration to allow all origins
-CORS(app, resources={r"/api/*": {"origins": ["http://localhost:8081", "https://your-frontend-domain.com"]}})
+CORS(app, resources={r"/api/*": {"origins": ["https://gel-card-classifier-60152oeqg-albaraazains-projects.vercel.app"]}})
 
 # Load the trained model, scaler, and label encoder
 model = joblib.load('trained_model.joblib')
